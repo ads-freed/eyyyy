1a1b9c7e6fa13f74db68ca7da89764ed8daaf07a
ads-freed <57936630+ads-freed@users.noreply.github.com>
2025-03-20 09:57:22 +0800

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -9,5 +9,23 @@ class CustomUser(AbstractUser):
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='customer')
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='customuser_set',
+        related_query_name='customuser'
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='customuser_set',
+        related_query_name='customuser'
+    )
+
     def __str__(self):
         return self.username
